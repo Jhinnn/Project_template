2143a5b7319f5a79f21b4e55f6d64334615010e2
Bronze_Pig <2979468570@qq.com>
2017-10-20 15:07:09 +0800

    first commit

---
+++ MVVM/Base/BaseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseTableViewController.h
+//  MVVM
+//
+//  Created by Arthur on 2017/10/20.
+//  Copyright © 2017年 Arthur. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseTableViewController : BaseViewController
+
+@end
---
+++ MVVM/Base/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  MVVM
+//
+//  Created by Arthur on 2017/10/20.
+//  Copyright © 2017年 Arthur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
